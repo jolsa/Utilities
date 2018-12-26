f77f04ea0365fa1c4da096c5b39545a1e5c53cd4
Johnny Olsa <johnny@johnnyolsa.com>
2018-12-25 19:37:13 -0600

    ImageRotation, GetBytesRaw, GetFrameCount

--- Images/ImageExtensions.cs
+++ Images/ImageExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
@@ -6,8 +7,32 @@ using Utilities.Images;
 
 namespace System.Drawing
 {
+	public enum Rotations
+	{
+		RotateNone = 0,
+		Rotate90 = 1,
+		Rotate180 = 2,
+		Rotate270 = 3
+	}
 	public static class ImageExtensions
 	{
+		private static readonly Dictionary<Rotations, RotateFlipType> _rotationMap = new Dictionary<Rotations, RotateFlipType>()
+		{
+			[Rotations.RotateNone] = RotateFlipType.RotateNoneFlipNone,
+			[Rotations.Rotate90] = RotateFlipType.Rotate90FlipNone,
+			[Rotations.Rotate180] = RotateFlipType.Rotate180FlipNone,
+			[Rotations.Rotate270] = RotateFlipType.Rotate270FlipNone
+		};
+		private static readonly HashSet<Rotations> _rotations = new HashSet<Rotations>(Enum.GetValues(typeof(Rotations)).Cast<Rotations>());
+		public static Bitmap Rotate(this Bitmap bitmap, Rotations rotation)
+		{
+			if (!_rotations.Contains(rotation))
+				throw new ArgumentException($"Invalid {nameof(rotation)} value of {rotation}");
+			var img2 = (Image)bitmap.Clone();
+			if (rotation != Rotations.RotateNone)
+				img2.RotateFlip(_rotationMap[rotation]);
+			return (Bitmap)img2;
+		}
 		public static byte[] GetBytes(this Bitmap bmp) => GetBytes(bmp, ImageFormat.Jpeg);
 		public static byte[] GetBytes(this Bitmap bmp, ImageFormat format)
 		{
@@ -17,6 +42,15 @@ namespace System.Drawing
 				return mem.GetBuffer();
 			}
 		}
+		public static byte[] GetBytesRaw(this Bitmap bmp, ImageFormat rawFormat)
+		{
+			using (var mem = new MemoryStream())
+			{
+				bmp.Save(mem, ImageFilesHelper.ImageFormatFromRawFormat(rawFormat));
+				return mem.GetBuffer();
+			}
+		}
+		public static Bitmap ToBitmap(this byte[] data) => (Bitmap)Image.FromStream(new MemoryStream(data));
 
 		public static ImageCodecInfo GetCodecInfo(this Image img) =>
 			ImageCodecInfo.GetImageDecoders().FirstOrDefault(decoder => decoder.FormatID == img.RawFormat.Guid);
@@ -29,6 +63,8 @@ namespace System.Drawing
 		public static ImageCodecInfoPlus GetCodecInfoPlus(this Image img) => new ImageCodecInfoPlus(img);
 
 		public static ImageCodecInfoPlus GetCodecInfoPlus(this string file) => new ImageCodecInfoPlus(file);
+
+		public static int GetFrameCount(this Image image) => image.GetFrameCount(new FrameDimension(image.FrameDimensionsList.First()));
 
 	}
 }
